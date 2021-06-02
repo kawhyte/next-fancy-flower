5ba6be40c84a9678273161547b04a9a4320d32d8
Kenny Whyte <kawhyte@gmail.com>
2021-06-02 13:38:45 -0700

    all

--- pages/all.js
+++ pages/all.js
@@ -8,10 +8,14 @@ const space = process.env.CONTENTFUL_SPACE_ID;
 const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 import { createClient } from "contentful";
-import Hero from "@components/Hero";
-import CustomizationOffer from "@components/CustomizationOffer";
-import OurStory from "@components/OurStory";
-import Categories from "@components/Categories";
+import Masonry from "react-masonry-css";
+
+const breakpointColumnsObj = {
+	default: 3,
+	1100: 2,
+	700: 1,
+	500: 1,
+};
 
 function AllCakes({ posts }) {
 	console.log(posts);
@@ -19,7 +23,7 @@ function AllCakes({ posts }) {
 	return (
 		<Layout color={false}>
 			<Head>
-				<title>Birthday Cakes | Fancyflower</title>
+				<title>All Cakes | Fancyflower</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
@@ -27,42 +31,38 @@ function AllCakes({ posts }) {
 			<section class='text-gray-600 body-font'>
 				<div class='container px-5 py-24 mx-auto'>
 					<div class='flex flex-col text-center w-full mb-20'>
-
-          <div className='flex justify-center'>
-          <p className='p-2 mb-8 text-xl font-extrabold tracking-tight text-center text-black  leading-8 sm:text-4xl sm:leading-10'>
-            All Cakes
-            <div className='flex justify-start w-40 py-0 mx-auto my-0 border-b-4 rounded-t opacity-50 border-yellow-500 gradient  sm:w-72'></div>
-          </p>
-        </div>
-  
-	
-			
+						<div className='flex justify-center'>
+							<p className='p-2 mb-8 text-xl font-extrabold tracking-tight text-center text-black  leading-8 sm:text-4xl sm:leading-10'>
+								All Cakes
+								<div className='flex justify-start w-40 py-0 mx-auto my-0 border-b-4 rounded-t opacity-50 border-yellow-500 gradient  sm:w-72'></div>
+							</p>
+						</div>
 					</div>
-
-					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 auto-cols-min '>
+					<Masonry
+						breakpointCols={breakpointColumnsObj}
+						className='my-masonry-grid'
+						columnClassName='my-masonry-grid_column'>
 						{posts.map((item) => (
 							<>
-								<div className=''>
-									<div class='   '>
-										<Image
-											class=' object-cover shadow-lg rounded-3xl '
-											src={"https:" + item.fields.thumbnail.fields.file.url}
-											width={
-												item.fields.thumbnail.fields.file.details.image.width
-											}
-											height={
-												item.fields.thumbnail.fields.file.details.image.height
-											}
-										/>
-									</div>
+								<div className='relative'>
+									<span class='px-4 py-2 my-4 mx-4  text-base rounded-full text-black  bg-yellow-300  absolute z-10 '>
+										{item.fields.name}
+									</span>
 
-									<h1 class='title-font text-sm lg:text-lg font-medium text-black bg-white pl-3  '>
-										{item.fields.name} 1
-									</h1>
+									<Image
+										class=' object-cover shadow-lg rounded-3xl absolute'
+										src={"https:" + item.fields.thumbnail.fields.file.url}
+										width={
+											item.fields.thumbnail.fields.file.details.image.width
+										}
+										height={
+											item.fields.thumbnail.fields.file.details.image.height
+										}
+									/>
 								</div>
 							</>
 						))}
-					</div>
+					</Masonry>
 				</div>
 			</section>
 		</Layout>
