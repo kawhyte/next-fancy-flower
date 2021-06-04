178f17148ccf4952abf4f5c7dab0ce3224deaa5d
Kenny Whyte <kawhyte@gmail.com>
2021-06-04 11:16:12 -0700

    yellow

--- components/Pages.js
+++ components/Pages.js
@@ -11,11 +11,11 @@ const breakpointColumnsObj = {
 
 
 function Pages({posts, heading}) {
-   console.log("Pages ", posts)
+  //console.log("Pages ", posts)
     return (
-        <section class='text-gray-600 body-font'>
-        <div class='container py-12 px-5 lg:py-24 mx-auto'>
-            <div class='flex flex-col text-center w-full mb-10 lg:mb-20'>
+        <section className='text-gray-600 body-font'>
+        <div className='container py-12 px-5 lg:py-24 mx-auto'>
+            <div className='flex flex-col text-center w-full mb-10 lg:mb-20'>
                 <div className='flex justify-center'>
                     <p className='p-2 mb-8 text-xl font-extrabold tracking-tight text-center text-black  leading-8 sm:text-4xl sm:leading-10'>
                         {heading}
@@ -29,13 +29,13 @@ function Pages({posts, heading}) {
                 columnClassName='my-masonry-grid_column'>
                 {posts.map((item) => (
                    
-                        <div key={item.sys.id} className='relative'>
+                        <div key={item.sys.id} className='relative bg-yellow-50  backdrop-filter backdrop-blur-lg rounded-3xl p-1 '>
                             <span class='px-4 py-2 -my-4 mx-4  text-xs sm:text-sm md:text-base text-black  bg-yellow-200  absolute z-10 '>
                                 {item.fields.name}
                             </span>
 
                             <Image
-                                class=' object-cover shadow-lg rounded-3xl absolute'
+                                class=' object-cover rounded-3xl absolute'
                                 src={"https:" + item.fields.thumbnail.fields?.file?.url}
                                 width={
                                     item.fields.thumbnail?.fields?.file.details.image.width
